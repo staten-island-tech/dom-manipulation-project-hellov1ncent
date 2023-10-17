d2eff3062117087dd455276b2aa1c12246381f85
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-10-17 10:08:40 -0400

    dd

--- dom.js
+++ dom.js
@@ -1,10 +1,15 @@
 const DOMSelectors = {
-    form: document.querySelector("form"),
-    firstName: document.querySelector(".firstname"),
-    h2s: document.querySelectorAll("h2"),
+    button: document.getElementById("btn"),
+    box: document.getElementById("container-box"),
+    input: document.querySelectorA("#input"),
 
 }
-DOMSelectors.form.addEventListener("submit", function (event){
-    event.preventDefault();
-    console.log
-})
+
+
+DOMSelectors.button.addEventListener("click", function(){
+    let input = DOMSelectors.input.value;
+    DOMSelectors.box.insertAdjacentHTML("beforeend", <p>${input}</p>);
+    DOMSelectors.input.value = ";"
+}
+);
+
