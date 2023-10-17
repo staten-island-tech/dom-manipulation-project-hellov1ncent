bb5e44af9fdd34a82452aad6d796e98cc5be7b39
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-10-17 10:11:38 -0400

    sds

--- dom.js
+++ dom.js
@@ -1,9 +1,9 @@
 const DOMSelectors = {
     button: document.getElementById("btn"),
     box: document.getElementById("container-box"),
-    input: document.querySelectorA("#input"),
+    input: document.querySelector("#input"),
 
-}
+};
 
 
 DOMSelectors.button.addEventListener("click", function(){
