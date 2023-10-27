fe1918982d3628ff8e50e92754c39ad34933b00b
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-10-26 18:58:12 -0600

    saas

--- dom.js
+++ dom.js
@@ -5,5 +5,18 @@ const DOMSelectors = {
     button: document.getElementById("btn"),
     input: document.querySelector(#input),
     input2: document.querySelector(#input2),
-    delete: document.querySelector(#delete);
+    delete: document.querySelector(#delete)
 }
+
+
+const card = DOMSelectors.button.addEventListener("click", function(){
+    let input= DOMSelectors.input.value;
+    let input2= DOMSelectors.input2.value;});
+    DOMSelectors.form.insertAdjacentHTML("beforeend"),
+
+        `<div class="gallery">
+        <div class="card">
+            <h2 class="card-header" id="column">${input}</h2>
+            <h3 class="card-info" id="column">${input2}</h3>
+        </div>
+        <button type="submit" id="delete">Delete</button>); 
