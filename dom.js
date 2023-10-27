7ba0bc58f30152071107aa5b21d66d5fa5b17b46
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-10-26 18:25:56 -0600

    sds

---
+++ dom.js
@@ -0,0 +1,9 @@
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    h2s: document.querySelectorAll("h2"),
+    h3s: document.querySelectorAll("h3"),
+    button: document.getElementById("btn"),
+    input: document.querySelector(#input),
+    input2: document.querySelector(#input2),
+    delete: document.querySelector(#delete);
+}
