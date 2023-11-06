b39d335bcf6fb49c11f78029d5590e8883271e62
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-11-06 10:14:41 -0500

    dssds

--- dom.js
+++ dom.js
@@ -1,22 +1,23 @@
 const DOMSelectors = {
-    form: document.querySelector("#form"),
-    h2s: document.querySelectorAll("h2"),
-    h3s: document.querySelectorAll("h3"),
     button: document.getElementById("btn"),
-    input: document.querySelector(#input),
-    input2: document.querySelector(#input2),
-    delete: document.querySelector(#delete)
-}
+    input: document.querySelector("#input"),
+    input2: document.querySelector("#input2"),
+    delete: document.querySelector("#delete"),
+    delete: document.querySelector("#delete"),
+    h2s: document.querySelector("#h2"),
+    h2s: document.querySelector("#h3"),
+
+};
 
 
-const card = DOMSelectors.button.addEventListener("click", function(){
+const card = DOMSelectors.button.addEventListener("click", function(event){event.preventDefault();
     let input= DOMSelectors.input.value;
     let input2= DOMSelectors.input2.value;});
-    DOMSelectors.form.insertAdjacentHTML("beforeend"),
+    DOMSelectors.form.insertAdjacentHTML("beforeend",
 
         `<div class="gallery">
         <div class="card">
             <h2 class="card-header" id="column">${input}</h2>
             <h3 class="card-info" id="column">${input2}</h3>
         </div>
-        <button type="submit" id="delete">Delete</button>)
+        <button type="submit" id="delete">Delete</button>);`)
