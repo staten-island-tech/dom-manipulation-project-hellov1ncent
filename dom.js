66bab81c1661b48607e26d801e33da30efbeb7a2
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-11-08 09:25:42 -0700

    sdsdsds

--- dom.js
+++ dom.js
@@ -19,8 +19,9 @@ DOMSelectors.form.addEventListener("submit", function(event){
     let input= DOMSelectors.input.value;
     let input2= DOMSelectors.input2.value;
     let input3 = DOMSelectors.input3.value;
-    DOMSelectors.parent.insertAdjacentHTML("beforeend",
-    `<div class="card">
+    DOMSelectors.form.insertAdjacentHTML("beforeend",
+    `<div class="column">
+    <div class="card">
         <h2 class="card-header" id="column">${input}</h2>
         <img src = "${input3}" alt = "" class = "img">
         <h3 class="card-info" id="column">By: ${input2}</h3>
